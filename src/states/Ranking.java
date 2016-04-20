0726e32e3314759c650936bf4ed927cdc6e09f07
mellowdeep <mellowdeep@abv.bg>
2016-04-20 11:18:33 +0300

    ranking

---
+++ src/states/Ranking.java
@@ -0,0 +1,69 @@
+package states;
+
+import java.io.*;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by Anton on 20-Apr-16.
+ */
+public class Ranking {
+
+    private int score;
+    private String name;
+    private String line;
+
+    public Ranking(String name, int score) {
+        this.name = name;
+        this.score = score;
+
+        TreeMap<Integer, String> rank = new TreeMap<>();
+
+
+        int first = Integer.parseInt(rank.get(1).split(" ")[0]);
+        int last = Integer.parseInt(rank.get(3).split(" ")[0]);
+        try (BufferedReader reader = new BufferedReader(new FileReader(
+                new File("resources/ranking.txt")));) {
+            while ((line = reader.readLine()) != null) {
+
+                int result = Integer.parseInt(line.split(" ")[0]);
+                String user = line.split(" ")[1];
+
+                if (result <= last) {
+                    break;
+                }
+
+                if (result > last && result < first) {
+                    int replace = Integer.parseInt(rank.get(2).split(" ")[0]);
+                    String replaceName = rank.get(2).split(" ")[1];
+                    rank.remove(3);
+                    rank.put(2, result + " " + user);
+                    rank.put(3, replace + " " + replaceName);
+                } else {
+                    int replaceFirst = Integer.parseInt(rank.get(1).split(" ")[0]);
+                    String replaceFirstName = rank.get(1).split(" ")[1];
+                    int replaceSec = Integer.parseInt(rank.get(2).split(" ")[0]);
+                    String replaceSecondName = rank.get(2).split(" ")[1];
+                    rank.remove(3);
+                    rank.put(1, result + " " + user);
+                    rank.put(2,replaceFirst + " " + replaceFirstName);
+                    rank.put(3, replaceSec + " " + replaceSecondName);
+                }
+
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(
+                    "resources/ranking.txt"));
+
+            for (Map.Entry<Integer, String> data : rank.entrySet()) {
+                writer.write(data.getKey() + " " + data.getValue());
+            }
+            writer.flush();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
